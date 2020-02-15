3f3410ffa476f251b88e892f9f0b08093c8f998d
IN-ST-027 <steph.lalande@free.fr>
2020-02-15 14:36:47 +0100

    Creation projet

---
+++ src/main/java/com/adaming/gestionclinique/GestionClinique2Application.java
@@ -0,0 +1,13 @@
+package com.adaming.gestionclinique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClinique2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionClinique2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/adaming/gestionclinique/GestionClinique2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaming.gestionclinique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClinique2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
